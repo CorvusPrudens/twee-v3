54cd76fc91ad1500733edcca1ad5cb369f4925f5
Redwarp <redwarp@gmail.com>
2023-01-28 01:53:55 +0100

    Add TryFrom<String> as well

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,14 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
 #[derive(Debug)]
-pub enum ParsingError<'a> {
-    Parsing(&'a str),
+pub enum ParsingError<T> {
+    Parsing(T),
 }
 
-impl<'a> Display for ParsingError<'a> {
+impl<T> Display for ParsingError<T>
+where
+    T: Display,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ParsingError::Parsing(error) => f.write_fmt(format_args!("{error}")),
@@ -13,14 +16,24 @@ impl<'a> Display for ParsingError<'a> {
     }
 }
 
-impl<'a> std::error::Error for ParsingError<'a> {}
+impl<T> std::error::Error for ParsingError<T> where T: Display + Debug {}
 
-impl<'a> From<nom::Err<nom::error::Error<&'a str>>> for ParsingError<'a> {
+impl<'a> From<nom::Err<nom::error::Error<&'a str>>> for ParsingError<&'a str> {
     fn from(value: nom::Err<nom::error::Error<&'a str>>) -> Self {
         match value {
             nom::Err::Incomplete(_) => ParsingError::Parsing("Incomplete parsing"),
             nom::Err::Error(e) => ParsingError::Parsing(e.input),
             nom::Err::Failure(e) => ParsingError::Parsing(e.input),
+        }
+    }
+}
+
+impl From<nom::Err<nom::error::Error<&str>>> for ParsingError<String> {
+    fn from(value: nom::Err<nom::error::Error<&str>>) -> Self {
+        match value {
+            nom::Err::Incomplete(_) => ParsingError::Parsing("Incomplete parsing".to_string()),
+            nom::Err::Error(e) => ParsingError::Parsing(e.input.to_string()),
+            nom::Err::Failure(e) => ParsingError::Parsing(e.input.to_string()),
         }
     }
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -7,11 +7,36 @@ pub(crate) mod passage;
 pub(crate) mod story;
 
 impl<'a> TryFrom<&'a str> for Story<&'a str> {
-    type Error = ParsingError<'a>;
+    type Error = ParsingError<&'a str>;
 
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         match parse_story(value) {
             Ok((_, story)) => Ok(story),
+            Result::Err(error) => Result::Err(error.into()),
+        }
+    }
+}
+
+impl TryFrom<String> for Story<String> {
+    type Error = ParsingError<String>;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match parse_story(&value) {
+            Ok((_, story)) => {
+                let Story {
+                    content: _,
+                    title,
+                    start,
+                    passages,
+                } = story;
+
+                Ok(Story {
+                    content: value,
+                    title,
+                    start,
+                    passages,
+                })
+            }
             Result::Err(error) => Result::Err(error.into()),
         }
     }
